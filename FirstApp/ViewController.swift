e8377e87cc0fb9ec4d9e80402923104ce3f3ca26
Amanda Schlarman <Amanda.schlarman@gmail.com>
2019-06-05 20:40:22 -0500

    Initial Commit

---
+++ FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstApp
+//
+//  Created by Amanda Schlarman on 6/5/19.
+//  Copyright © 2019 Amanda Schlarman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
