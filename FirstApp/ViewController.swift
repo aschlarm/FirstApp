44b91ad66e506f0f7c27d6c33ebcb750c4ad547e
Amanda Schlarman <Amanda.schlarman@gmail.com>
2019-06-05 23:01:30 -0500

    Initial Commit

--- FirstApp/ViewController.swift
+++ FirstApp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var Name: UITextField!
+    @IBOutlet weak var Welcome: UILabel!
+   
+    
+    @IBAction func ButtonClick(_ sender: UIButton) {
+        print("Button Pressed")
+        if Name.text != " " {
+            label.text = Name.text
+            let TempName = Name.text
+            Welcome.text = "Hello " + TempName!
+            
+            if Welcome.isHidden {
+                Welcome.isHidden = false
+            }
+            else {
+                Welcome.isHidden = true
+            }
+        } else {
+            label.text = "Please type your name and try again"
+            Welcome.isHidden = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
